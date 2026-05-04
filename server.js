68fba86878574c1bde0b2ab7d60e49bf777f3f88
Apple <Apple@MacBook-Air.local>
2026-05-04 04:53:33 -0700

    added some new codes

--- server.js
+++ server.js
@@ -28,4 +28,11 @@ app.use(express.static(path.join(__dirname, "frontend")))
 
 // Start server
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(` Server running on port ${PORT}`))
+// app.listen(PORT, () => console.log(` Server running on port ${PORT}`))
+
+
+app.get("/", (req, res) => {
+
+  res.send("API running")
+
+})
